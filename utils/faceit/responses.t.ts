65e6ab9044a6c3eaef23b0344838639a6a5455fc
Jani Sundqvist <37551140+Sunquist@users.noreply.github.com>
2024-08-21 13:37:23 +0300

    New types

--- utils/faceit/responses.t.ts
+++ utils/faceit/responses.t.ts
@@ -139,3 +139,73 @@ interface Summary {
     ranking: number,
     image: string
 }
+
+interface MatchIdentifier {
+    matchId: string;
+    playerId: string;
+}
+
+interface MatchStats {
+    _id: MatchIdentifier;
+    created_at: number;
+    updated_at: number;
+    i31: string;
+    i30: string;
+    i33: string;
+    i10: string;
+    i32: string;
+    i13: string;
+    i35: string;
+    i34: string;
+    i37: string;
+    i15: string;
+    i36: string;
+    i14: string;
+    i39: string;
+    i38: string;
+    i16: string;
+    nickname: string;
+    playerId: string;
+    i40: string;
+    i20: string;
+    i22: string;
+    i21: string;
+    i24: string;
+    i23: string;
+    i26: string;
+    i6: string;
+    i25: string;
+    i28: string;
+    i7: string;
+    i8: string;
+    i27: string;
+    i9: string;
+    i29: string;
+    c10: string;
+    c2: string;
+    c3: string;
+    c4: string;
+    c1: string;
+    i19: string;
+    teamId: string;
+    i3: string;
+    i4: string;
+    i5: string;
+    premade: boolean;
+    c5: string;
+    bestOf: string;
+    competitionId: string;
+    date: number;
+    game: string;
+    gameMode: string;
+    i0: string;
+    i1: string;
+    i12: string;
+    i18: string;
+    i2: string;
+    matchId: string;
+    matchRound: string;
+    played: string;
+    status: string;
+    elo: string;
+}
